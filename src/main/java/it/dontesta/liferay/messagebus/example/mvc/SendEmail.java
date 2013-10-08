8daa0b2c4c2549941cf5f00409c8a774196ce1e7
Antonio Musarra <antonio.musarra@gmail.com>
2013-10-08 22:50:20 +0200

    Updated

--- src/main/java/it/dontesta/liferay/messagebus/example/mvc/SendEmail.java
+++ src/main/java/it/dontesta/liferay/messagebus/example/mvc/SendEmail.java
@@ -6,6 +6,7 @@ package it.dontesta.liferay.messagebus.example.mvc;
 import java.io.IOException;
 import java.util.List;
 
+import javax.mail.internet.InternetAddress;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
@@ -16,6 +17,10 @@ import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.mail.MailMessage;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -31,12 +36,12 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * @author amusarra
- *
+ * 
  */
 public class SendEmail extends MVCPortlet {
 
 	/**
-	 * Send a list of no active user
+	 * Send a list of no active user by SubscriptionSender
 	 * 
 	 * @param actionRequest
 	 * @param actionResponse
@@ -44,22 +49,104 @@ public class SendEmail extends MVCPortlet {
 	 * @throws PortletException
 	 * @throws SystemException
 	 */
-	public void sendUserListNoActive(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws IOException, PortletException, SystemException {
+	public void sendEmailBySubscriptionSender(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException,
+			PortletException, SystemException {
 
 		User user = (User) actionRequest.getAttribute(WebKeys.USER);
+
+		List<User> userList = getInactiveUsers(user);
+		String emailBody = getEmailBody(userList);
+
+		if (userList.size() > 0) {
+			SubscriptionSender subscriptionSender = new SubscriptionSender();
+
+			subscriptionSender.setSubject("List of disabled users");
+			subscriptionSender.setBody(emailBody.toString());
+			subscriptionSender.setUserId(user.getUserId());
+			subscriptionSender.setCompanyId(user.getCompanyId());
+			subscriptionSender.setFrom("noreply@liferay.com", "Liferay Portal");
+			subscriptionSender.setHtmlFormat(false);
+			subscriptionSender.setMailId("user", user.getUserId());
+
+			subscriptionSender.addRuntimeSubscribers(user.getEmailAddress(),
+					user.getFullName());
+
+			List<EmailAddress> emails = (List<EmailAddress>) user
+					.getEmailAddresses();
+			if (emails.size() > 0) {
+				if (_log.isInfoEnabled()) {
+					_log.info("User " + user.getUserId()
+							+ " has additional emails address");
+				}
+				for (EmailAddress emailAddress : emails) {
+					subscriptionSender.addRuntimeSubscribers(
+							emailAddress.getAddress(),
+							(String) user.getFullName());
+
+				}
+			}
+
+			subscriptionSender.flushNotificationsAsync();
+			SessionMessages.add(actionRequest,
+					"email-userlist-no-active-send-successfully");
+		} else {
+			SessionErrors.add(actionRequest,
+					"email-userlist-no-active-count-zero");
+		}
+	}
+
+	/**
+	 * Send a list of no active user by Email Sender
+	 * 
+	 * @param actionRequest
+	 * @param actionResponse
+	 * @throws IOException
+	 * @throws PortletException
+	 * @throws SystemException
+	 */
+	public void sendEmailByEmailSender(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException,
+			PortletException, SystemException {
+		User user = (User) actionRequest.getAttribute(WebKeys.USER);
+
+		List<User> userList = getInactiveUsers(user);
+		String emailBody = getEmailBody(userList);
+
+		if (userList.size() > 0) {
+			InternetAddress from = new InternetAddress("noreply@liferay.com",
+					"Liferay Portale");
+			InternetAddress to = new InternetAddress(user.getEmailAddress(),
+					user.getFullName());
+
+			MailMessage message = new MailMessage(from, to,
+					"List of disabled users", emailBody, false);
+			
+			Message myMessage = new Message();
+			myMessage.setDestinationName(DestinationNames.MAIL);
+			myMessage.setPayload(message);
+			MessageBusUtil.sendMessage(myMessage.getDestinationName(), myMessage);
+
+			SessionMessages.add(actionRequest,
+					"email-userlist-no-active-send-successfully");
+		} else {
+			SessionErrors.add(actionRequest,
+					"email-userlist-no-active-count-zero");
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param userList
+	 * @return
+	 */
+	protected String getEmailBody(List<User> userList) {
 		StringBundler emailBody = new StringBundler();
-		
-		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(User.class);
-		dynamicQuery.add(PropertyFactoryUtil.forName("status").eq(WorkflowConstants.STATUS_INACTIVE));
-		dynamicQuery.add(PropertyFactoryUtil.forName("companyId").eq(user.getCompanyId()));
-		
-		List<User> userList = UserLocalServiceUtil.dynamicQuery(dynamicQuery);
-		
-		emailBody.append("A seguire la lista degli utenti disattivati:");
+
+		emailBody.append("Below is the list of disabled users:");
 		emailBody.append(StringPool.NEW_LINE);
-		
+
 		for (User detailUser : userList) {
 			emailBody.append(StringPool.POUND);
 			emailBody.append(StringPool.SPACE);
@@ -71,44 +158,29 @@ public class SendEmail extends MVCPortlet {
 							.getLastLoginDate() : StringPool.BLANK);
 			emailBody.append(StringPool.NEW_LINE);
 		}
-		
-		if (userList.size() > 0) {
-			SubscriptionSender subscriptionSender = new SubscriptionSender();
 
-			subscriptionSender.setSubject("Lista degli utenti disattivati");
-			subscriptionSender.setBody(emailBody.toString());
-			subscriptionSender.setUserId(user.getUserId());
-			subscriptionSender.setCompanyId(user.getCompanyId());
-			subscriptionSender.setFrom("noreply@liferay.com", "Liferay Portal");
-			subscriptionSender.setHtmlFormat(false);
-			subscriptionSender.setMailId("user", user.getUserId());
+		return emailBody.toString();
 
-			subscriptionSender.addRuntimeSubscribers(
-				user.getEmailAddress(),
-				user.getFullName());
-
-			List<EmailAddress> emails = (List<EmailAddress>) user.getEmailAddresses();
-			if (emails.size() > 0) {
-				if (_log.isInfoEnabled()) {
-					_log.info("User " + user.getUserId() +
-						" has additional emails address");
-				}
-				for (EmailAddress emailAddress : emails) {
-					subscriptionSender.addRuntimeSubscribers(
-						emailAddress.getAddress(),
-						(String) user.getFullName());
-					
-				}
-			}
-			
-			subscriptionSender.flushNotificationsAsync();
-
-			SessionMessages.add(actionRequest, "email-userlist-no-active-send-successfully");
-		} else {
-			SessionErrors.add(actionRequest, "email-userlist-no-active-count-zero");
-		}
 	}
-	
-	private static Log _log =
-			LogFactoryUtil.getLog(SendEmail.class);
+
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 * @throws SystemException
+	 */
+	@SuppressWarnings("unchecked")
+	protected List<User> getInactiveUsers(User user) throws SystemException {
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil
+				.forClass(User.class);
+		dynamicQuery.add(PropertyFactoryUtil.forName("status").eq(
+				WorkflowConstants.STATUS_INACTIVE));
+		dynamicQuery.add(PropertyFactoryUtil.forName("companyId").eq(
+				user.getCompanyId()));
+
+		List<User> userList = UserLocalServiceUtil.dynamicQuery(dynamicQuery);
+		return userList;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(SendEmail.class);
 }
--- src/main/webapp/view.jsp
+++ src/main/webapp/view.jsp
@@ -14,20 +14,32 @@
  */
 --%>
 
+<%@page import="com.liferay.portal.kernel.language.LanguageUtil"%>
 <%@ include file="/init.jsp"%>
 
-<portlet:actionURL name="sendUserListNoActive" var="sendUserListNoActive"/>
+<portlet:actionURL name="sendEmailBySubscriptionSender" var="sendEmailBySubscriptionSender"/>
+<portlet:actionURL name="sendEmailByEmailSender" var="sendEmailByEmailSender"/>
+
 <liferay-ui:success key="email-userlist-no-active-send-successfully" message="email-userlist-no-active-send-successfully" />
 <liferay-ui:error key="email-userlist-no-active-count-zero" message="email-userlist-no-active-count-zero" />
 
 This is the <b>liferay-messagebus-subscribe-example</b>.
 
-<aui:form name="fmsendUserListNoActive"
-		action="<%=sendUserListNoActive.toString()%>" method="post">
+<aui:form name="<portlet:namespace/>fmSendEmailBySubscriptionSender"
+		action="<%=sendEmailBySubscriptionSender.toString()%>" method="post">
 		<aui:fieldset>
 			<aui:button-row>
-				<aui:button value="Send List of No Active User" type="submit" />
+				<aui:button  value='<%=LanguageUtil.get(portletConfig, locale, "send-email-by-subscriptionsender") %>' type="submit" />
 			</aui:button-row>
 		</aui:fieldset>
 		
+</aui:form>
+
+<aui:form name="<portlet:namespace/>fmSendEmailByEmailSender"
+		action="<%=sendEmailByEmailSender.toString()%>" method="post">
+		<aui:fieldset>
+			<aui:button-row>
+				<aui:button  value='<%=LanguageUtil.get(portletConfig, locale, "send-email-by-email-sender") %>' type="submit" />
+			</aui:button-row>
+		</aui:fieldset>
 </aui:form>
